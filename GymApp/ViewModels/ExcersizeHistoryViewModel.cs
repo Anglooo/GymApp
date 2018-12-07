606154d93a79c19140ce6a12b950f0d9f9458fe8
Thomas Frow <tfrow@novarumdx.com>
2018-12-07 09:26:14 +0000

    added new repo for excersize and hooked history up to the repo

--- GymApp/ViewModels/ExcersizeHistoryViewModel.cs
+++ GymApp/ViewModels/ExcersizeHistoryViewModel.cs
@@ -1,4 +1,7 @@
-﻿using MvvmCross.Commands;
+﻿using System;
+using System.Collections.Generic;
+using GymApp.Model;
+using MvvmCross.Commands;
 using MvvmCross.Navigation;
 using MvvmCross.ViewModels;
 
@@ -14,6 +17,41 @@ namespace GymApp.ViewModels
         {
             _navigationService = navigationService;
             NewExcersizeCommand = new MvxCommand(() => { _navigationService.Navigate<NewExcersizeViewModel>(); });
+        }
+
+        MvxObservableCollection<Excersize> _excersizes;
+        public MvxObservableCollection<Excersize> Excersizes
+        {
+            get
+            {
+                return _excersizes;
+            }
+            set
+            {
+                _excersizes = value;
+                RaisePropertyChanged("Excersizes");
+            }
+       }
+
+
+        public override void Prepare()
+        {
+            base.Prepare();
+        }
+
+        public async override void Start()
+        {
+            base.Start();
+
+            try
+            {
+                List<Excersize> excersizesList = await App.ExcersizeDatabase.GetItemsAsync();
+                Excersizes = new MvxObservableCollection<Excersize>(excersizesList);
+            }
+            catch(Exception e)
+            {
+                int i = 0;
+            }
         }
     }
 }
