632671772ccdc6aadc037e2b570d1e8d3a1f6118
Thomas Frow <tfrow@novarumdx.com>
2018-12-07 08:44:30 +0000

    committing missing files

---
+++ GymApp.Android/MainActivity.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.OS;
+using MvvmCross.Forms.Platforms.Android.Core;
+using MvvmCross.Forms.Platforms.Android.Views;
+
+namespace GymApp.Droid
+{
+    [Activity(Label = "GymApp", MainLauncher = true, Theme = "@style/MainTheme", NoHistory = true)]
+    public class MainActivity : MvxFormsAppCompatActivity<MvxFormsAndroidSetup<App, FormsApp>, App, FormsApp>
+    {
+
+        protected override void OnCreate(Bundle bundle) 
+        { 
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+            base.OnCreate(bundle); 
+        }
+
+
+    }
+}
---
+++ GymApp.iOS/AppDelegate.cs
@@ -0,0 +1,15 @@
+﻿
+using Foundation;
+using MvvmCross.Forms.Platforms.Ios.Core;
+
+namespace GymApp.iOS
+{
+    // The UIApplicationDelegate for the application. This class is responsible for launching the 
+    // User Interface of the application, as well as listening (and optionally responding) to 
+    // application events from iOS.
+    [Register("AppDelegate")]
+    public partial class AppDelegate : MvxFormsApplicationDelegate<MvxFormsIosSetup<App, FormsApp>, App, FormsApp>
+    {
+        
+    }
+}
